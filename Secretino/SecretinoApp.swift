5096b35edc1ec1b8c43026431992dd7e89def32b
Nicolazic <nicolazictardy@gmail.com>
2025-06-20 12:27:32 +0200

    initial commit

---
+++ Secretino/SecretinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretinoApp.swift
+//  Secretino
+//
+//  Created by Nicolazic Tardy on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SecretinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
